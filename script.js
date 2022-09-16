4d4734a60b7a3f48a494612668861b9329b99897
Viraj Chavan <virajchavan308@gmail.com>
2022-09-16 15:46:23 +0530

    mobile version done and added javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+function hide_show(id_name1, id_name2) {
+  var block = document.getElementById(id_name1);
+  block.style.display = "none";
+  var block2 = document.getElementById(id_name2);
+  block2.style.display = "block";
+}
